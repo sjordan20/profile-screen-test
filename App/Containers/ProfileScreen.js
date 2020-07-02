aacbd2ccbfc8c656184e5d7894dff26000848b8f
sjordan20 <jordanstephen949@gmail.com>
2020-07-02 15:23:58 -0600

    alerts and icons

--- App/Containers/ProfileScreen.js
+++ App/Containers/ProfileScreen.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import { ScrollView, Text, View, Image } from 'react-native'
+import { ScrollView, Text, View, Image, Button, Alert } from 'react-native'
 import { connect } from 'react-redux'
 // Add Actions - replace 'Your' with whatever your reducer is called :)
 // import YourActions from '../Redux/YourRedux'
 import Icon from 'react-native-vector-icons/AntDesign'
+
 // Styles
 import styles from './Styles/ProfileScreenStyle'
 
@@ -14,16 +15,52 @@ class ProfileScreen extends Component {
   //   this.state = {}
   // }
 
+  leavingToFacebook = () =>
+    Alert.alert(
+      "You are Now Leaving to Facebook",
+      "My Alert Msg",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ],
+      { cancelable: false }
+    );
+
   render() {
     return (
       <ScrollView style={styles.container}>
         <View style={styles.header}>
           <Icon name="pluscircleo" size={30} color='#949494' />
-          <Text>Contact Details</Text>
+          <Text style={styles.headerText}>Contact Details</Text>
           <View style={styles.leftHeaderBox}>
             <Icon name='bells' size={30} color='#949494' />
-            <Image source={{ uri: 'https://pluspng.com/img-png/png-hd-of-puppies-so-cute-puppies-image-puppy-png-puppy-png-hd-1600.png' }} style={styles.headerPic} />
+            <Image source={{ uri: 'https://pluspng.com/img-png/png-hd-of-puppies-so-cute-puppies-image-puppy-png-puppy-png-hd-1600.png' }} style={{ width: 30, height: 30, resizeMode: 'contain' }} />
+          </View>
+        </View>
+        <View style={styles.profileContainer}>
+          <View style={styles.profilePicContainer}>
+            <Image source={{ uri: 'https://img.favpng.com/11/19/18/dog-cat-towel-puppy-pet-png-favpng-7uVBw0KAJ75pxvmdcDAzvVVD0.jpg' }} style={{ width: 150, height: 150, resizeMode: 'contain', borderRadius: 100 }} />
+            <Text style={styles.profileName}>
+              Nathan Garcia
+            </Text>
+            <Text style={styles.profileTitle}> UI/UX Designer</Text>
+          </View>
+          <View style={styles.socialLinksContainer}>
+            <Button
+              icon={
+                <Icon name='facebook-square' size={40} color='#949494' style={styles.socialIcon} />
+              }
+              onPress={this.leavingToFacebook}
+              title='facebook'
+            />
 
+            <Icon name='github' size={40} color='#949494' style={styles.socialIcon} />
+            <Icon name='twitter' size={40} color='#949494' style={styles.socialIcon} />
+            <Icon name='linkedin-square' size={40} color='#949494' style={styles.socialIcon} />
           </View>
 
         </View>
--- App/Containers/Styles/ProfileScreenStyle.js
+++ App/Containers/Styles/ProfileScreenStyle.js
@@ -14,19 +14,68 @@ export default StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingLeft: 10,
-    paddingRight: 10
+    paddingRight: 10,
+  },
 
+  headerText: {
+    fontSize: 25,
   },
 
   leftHeaderBox: {
+    height: '100%',
+    width: 60,
     display: 'flex',
-    flexDirection: "row"
+    flexDirection: "row",
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  profileContainer: {
+    width: '100%',
+    height: 700,
+    backgroundColor: '#aeaeaeae',
+    display: "flex",
+    alignItems: "center"
+  },
+
+  profilePicContainer: {
+    width: '95%',
+    height: 250,
+    backgroundColor: 'white',
+    marginTop: 20,
+    display: 'flex',
+    alignItems: 'center',
 
   },
-  headerPic: {
-    width: 25,
-    height: 25,
-    resizeMode: 'stretch',
-    backgroundColor: 'black'
-  }
+
+  profileName: {
+    fontSize: 22,
+    fontWeight: '500',
+    paddingBottom: 5
+  },
+
+  profileTitle: {
+    fontSize: 18,
+    color: '#aeaeae',
+    fontWeight: '500'
+
+  },
+
+  socialLinksContainer: {
+    width: '95%',
+    height: 60,
+    backgroundColor: 'white',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 5,
+
+  },
+
+  socialIcon: {
+    paddingRight: 10
+  },
+
+
 })
